4e17d873b97faead41321fc983fbd7c96071161b
silvakwan1 <kauans003@gmail.com>
2024-09-23 03:37:37 -0300

    adicionei um metodo a clase uzumaki

--- src/uzumaki.java
+++ src/uzumaki.java
@@ -1,11 +1,28 @@
 public class uzumaki extends ninja{
    
     public uzumaki (String nome , String clan, boolean biju){
-       super(nome, clan, biju);
+       super(nome, clan);
     }
 
     public void chacra(){
         System.out.println("clan uzumaki e conhecido por ter bastante chacra");
+    }
+    
+    public void chacra(int quantidade){
+        if (quantidade >= 5) {
+            System.out.println("rasengan gigante");
+        }else if (quantidade >= 2) {
+            System.out.println("mil clones das sombras");
+            
+        } else {
+            if (biju) {
+                System.out.println("tá na hora de usar a biju");
+                
+            }else{
+                System.out.println("não consegue usar nenhum jutso");
+                
+            }
+        }
     }
 
 }
